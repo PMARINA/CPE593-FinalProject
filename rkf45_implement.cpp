929d0ca041448f0bfa049dddda36cfe4a8296639
joeyrupert <joeyrupert731@gmail.com>
2020-12-06 22:18:23 -0500

    build!: Begin RKF45 implementation: templating
    - Added template methods for k's
    - Began implementation for 5th order estimate
    
    Began implementing RKF45

---
+++ rkf45_implement.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using std::cout;
+using std::endl;
+
+//y_n
+
+double yn(double t);
+
+//k equations
+
+double k1();
+double k2();
+double k3();
+double k4();
+double k5();
+double k6();
+
+//y_(n+1) =
+
+double yn1(double t) {
+    return (yn(t) + ((double)256/(double)216)
+}
