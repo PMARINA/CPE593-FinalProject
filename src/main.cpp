bd1f561e1db5b5c463ab255e2f087a22c404fb53
Coffey08980 <mets0417@yahoo.com>
2020-12-13 14:40:44 -0500

    refactor!: Template main for new math
    - see main.cpp for new alg

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,48 @@
 #include <iostream>
+#include <vector>
 
 #include "obj/planetary_object.hh"
 using std::cout;
 using std::endl;
+using std::vector;
+
+int main()
+{
+  vector<Planetary_Object *> *planets = Planetary_Object::read_config();
+
+  // TODO: RKF45 first 5 vel (only 4 required)
+  while (true)
+  {
+     /**
+      * 1. Predict( p(n+1) given velocity matrix in each planet)
+      * 2. Use gravity equations to get p'' given p(n+1) for each planet
+      * 3. use correction alg C'(n+1)
+      * 4. Use correction alg C(n+1)
+      */ 
+    for (int i = 0; i < planets->size(); i++)
+    {
+      // Predict( p(n+1) given velocity matrix in each planet)
+    }
+    for (int i = 0; i < planets->size(); i++)
+    {
+      //Use gravity equations to get p'' given p(n+1) for each planet
+    }
+    for (int i = 0; i < planets->size(); i++)
+    {
+      //use correction alg C'(n+1)
+    }
+    for (int i = 0; i < planets->size(); i++)
+    {
+      // Use correction alg C(n+1)
+    }
+  }
+}
+
+/*
 int main() {
   cout << "Starting" << endl;
   vector<Planetary_Object*>* planets = Planetary_Object::read_config();
   cout << "Finished" << endl;
   return 0;
 }
+*/
--- src/math/predictor_corrector.cpp
+++ src/math/predictor_corrector.cpp
@@ -15,21 +15,31 @@ We need to find a way to give each function to this (ie: velocity for each body)
 /* double function(double t, double p){ //function that gives derivative of p at time t
 } */
 
-void predictor(double &y, uint64_t &h, double &f0, double &f1, double &f2, double &f3, double &return_to){ //predictor: y, h step, f0 = fn, f1 = fn-1, f2 = fn-2, f3 = fn-3
-    return_to = (y + ((h/24.0) * (-9.0*f3 + 37.0*f2 - 59.0*f1 + 55.0*f0)));
+void predictor(double &initial_value, uint64_t &step, double &fn0, double &fn1, double &fn2, double &fn3, double &return_to){ //predictor: fn0 = fn, fn1 = fn-1, fn2 = fn-2, fn3 = fn-3
+    return_to = (initial_value + ((step/24.0) * (-9.0*fn3 + 37.0*fn2 - 59.0*fn1 + 55.0*fn0)));
 }
 
-void corrector(double &y, uint64_t &h, double &f0, double &f1, double &f2, double &f3, double &return_to){ //corrector: y, h step, f0 = fn, f1 = fn-1, f2 = fn-2, f3 = fn-3
-    return_to = (y + ((h/24.0) * (f2 - 5.0*f1 + 19.0*f0 + 9.0*f1)));
+void corrector(double &initial_value, uint64_t &step, double &fn0, double &fn1, double &fn2, double &f1, double &return_to){ //corrector: fn0 = fn, fn1 = fn-1, fn2 = fn-2, f1 = fn+1
+    return_to = (initial_value + ((step/24.0) * (fn2 - 5.0*fn1 + 19.0*fn0 + 9.0*f1)));
 }
 
 void predictor_corrector(double* initial_value, double* deriv_array, uint64_t &timestep, double &return_to){
 //deriv_array contains fn-3, fn-2, fn-1, fn
-
 //initial_value contains starting yn
 
+//do acc first
+//for acc: initial_value = last "real" value used for acc | deriv_array -> gives vel
+//for vel: initial_value = last "real" value used for vel | deriv_array -> gives pos
+for (int i = 0; i < 2; i++){ //loop 3 times for x,y,z
+    double prediction = predictor (initial_value, timestep, deriv_array[i][0], deriv_array[i][1], deriv_array[i][2], deriv_array[i][3]);
+    double correction = corrector (initial_value, timestep, deriv_array[i][0], deriv_array[i][1], deriv_array[i][2], deriv_array[i][3]);
+    return_to = correction;
+}
 
-/*     double t = 0; //initial time
+
+
+/*     
+    double t = 0; //initial time
     double h = 0.2; //step
     double f0 = 0, f1 = 0, f2 = 0, f3 = 0;
 
@@ -65,5 +75,6 @@ void predictor_corrector(double* initial_value, double* deriv_array, uint64_t &t
         f1 = f0;
         t++;
         f0 = function (t, p);
-    } */
+    } 
+*/
 }
