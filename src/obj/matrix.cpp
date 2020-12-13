c0c3ed63f2597d1067d3c1bb787b36481c90f7f8
Coffey08980 <mets0417@yahoo.com>
2020-12-13 15:39:07 -0500

    refactor: Rename vars, reset to 0-based indexing
    - Mostly a refactor, almost no other functional changes beyond indexing
    - Added & tested matrix to ensure it works (see ifdeffed code)
    
    Co-authored-by: PMARINA <bluerat33@gmail.com>

--- src/obj/matrix.cpp
+++ src/obj/matrix.cpp
@@ -1,51 +1,85 @@
 #include "../math/constants.cpp"
 #include <cstdint>
 #include <string>
+#include <string.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
 
 using std::string;
 class Matrix {
 private:
     double* data;
-    uint64_t x;
-    uint64_t y;
+    uint64_t timesteps_stored;
+    uint64_t dims;
+    uint64_t get_offset(const uint64_t& dimension, const uint64_t index){
+        return dimension*dims + index;
+    }
+    uint64_t* get_dim_index(const uint64_t& offset){
+        uint64_t* dim_arr = new uint64_t[2];
+        dim_arr[0] = offset/dims;
+        dim_arr[1] = offset%dims;
+        return dim_arr;
+    }
 public:
-    Matrix(uint64_t amount_to_archive = 5, uint64_t dimensions = num_dims) : x(amount_to_archive), y(dimensions) {
-        uint64_t counter = 0;
-        for (int i = 0; i < dimensions; i++) {
-            for (int j = 0; j < amount_to_archive; j++, counter++)
-                data[counter] = 0;
-        }
+    Matrix(uint64_t amount_to_archive = 5, uint64_t dimensions = num_dims) : timesteps_stored(amount_to_archive), dims(dimensions) {
+        uint64_t num_elements = dims*timesteps_stored;
+        data = new double[num_elements];
+        memset(data, 0, num_elements*sizeof(double));
     };
     void setAtPoint(uint64_t &dimension, uint64_t &index, double &input_data) {
-        uint64_t location = (((dimension - 1) * x) + index) - 1;
-        data[location] = input_data;
+        data[get_offset(dimension, index)] = input_data;
     }
     void setAtPoint(string &dimensionl, uint64_t &index, double &input_data) {
-        double dimension;
+        uint64_t dimension;
         if (dimensionl == "x") {
-            dimension = 1;
+            dimension = 0;
         } else if (dimensionl == "y") {
-            dimension = 2;
+            dimension = 1;
         } else if (dimensionl == "z") {
-            dimension = 3;
+            dimension = 2;
         }
-        uint64_t location = (((dimension - 1) * x) + index) - 1;
-        data[location] = input_data;
+        data[get_offset(dimension, index)] = input_data;
     }
     void retrieveAtPoint(uint64_t &dimension, uint64_t &index, double &output_data) {
-        uint64_t location = (((dimension - 1) * x) + index) - 1;
-        output_data = data[location];
+        output_data = data[get_offset(dimension, index)];
     }
     void retrieveAtPoint(string &dimensionl, uint64_t &index, double &output_data) {
-        double dimension;
+        uint64_t dimension;
         if (dimensionl == "x") {
-            dimension = 1;
+            dimension = 0;
         } else if (dimensionl == "y") {
-            dimension = 2;
+            dimension = 1;
         } else if (dimensionl == "z") {
-            dimension = 3;
+            dimension = 2;
         }
-        uint64_t location = (((dimension - 1) * x) + index) - 1;
-        output_data = data[location];
+        // uint64_t location = (((dimension - 1) * x) + index) - 1;
+        output_data = data[get_offset(dimension, index)];
     }
 };
+#if 0
+int main(){
+    const uint64_t x = 2;
+    const uint64_t y = 100;
+    Matrix m(2,100);
+    uint64_t dim, index;
+    dim = index = 0;
+    for(double i = 0; i<x*y; i++){
+        index++;
+        if(index>=y){index = 0;dim++;}
+        if(dim>=x){dim = 0;}
+        m.setAtPoint(dim, index, i);
+    }
+    dim = index = 0;
+    double output_var;
+    for(double i = 0; i<x*y; i++){
+        index++;
+        if(index>=y){index = 0;dim++;}
+        if(dim>=x){dim = 0;}
+        m.retrieveAtPoint(dim, index, output_var);
+        cout << i <<": " << output_var<<endl;
+    }
+    return 0;
+}
+#endif
